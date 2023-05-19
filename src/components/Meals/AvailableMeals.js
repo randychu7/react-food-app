63732459986d3df8a699642b58cb169cae39c697
Randy Chu <randychu7@gmail.com>
2023-05-19 18:03:23 -0500

    meal form

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
 import Card from '../UI/Card'
+import MealItem from './MealItem/MealItem'
 import classes from './AvailableMeals.module.css'
 
 const DUMMY_MEALS = [
@@ -30,16 +31,20 @@ const DUMMY_MEALS = [
 
 
   const AvalibleMeals = () =>{
-    const mealsList = DUMMY_MEALS.map(meal => 
-        <li>{meal.name}</li>
+    const mealsList = DUMMY_MEALS.map((meal) => 
+        <MealItem 
+        key ={meal.id} 
+        name ={meal.name} 
+        description = {meal.description} 
+        price = {meal.price}/>
 
     );
 
     return <section className={classes.meals}>
         <Card>
-        <ul>
-            {mealsList}
-        </ul>
+            <ul>
+                {mealsList}
+            </ul>
         </Card>
     </section>
   }
