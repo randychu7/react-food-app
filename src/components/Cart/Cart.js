fa57cdc93500f4d67358064f00e8eef4fe8f45d2
Randy Chu <randychu7@gmail.com>
2023-05-19 21:06:20 -0500

    meal form

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,5 @@
 import classes from './Cart.module.css'
-
+import Modal from '../UI/Modal'
 const Cart = (props) =>{
 
 const cartItems = <ul className={classes['cart-items']}>{[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}
@@ -7,17 +7,17 @@ const cartItems = <ul className={classes['cart-items']}>{[{id: 'c1', name: 'Sush
 
 
 return (
-<div>
+<Modal onClose = {props.onClose}>
 {cartItems}
 <div className={classes.total}>
     <span>Total Amount</span>
     <span>35.62</span>
 </div>
 <div className={classes.actions}>
-    <button className={classes['button--alt']}>Close</button>
+    <button className={classes['button--alt']} onClick = {props.onClose}>Close</button>
     <button className={classes.button}>Order</button>
 </div>
-</div>)
+</Modal>)
 
 
 }
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,14 @@ import {Fragment} from "react"
 import mealsImage from "../../assets/meals.jpg"
 import classes from "./Header.module.css"
 import HeaderCartButton from './HeaderCartButton'
+
+
 const Header = (props) => {
 return <Fragment>
 
         <header className={classes.header}>
             <h1>BiteBuddy</h1>
-            <HeaderCartButton/>
+            <HeaderCartButton onClick={props.onShowCart}/>
         </header>
 
         <div className={classes['main-image']}>
