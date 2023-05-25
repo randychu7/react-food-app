f096f1ed035680a6a2e6d931212d3285cc979fb8
Randy Chu <randychu7@gmail.com>
2023-05-24 19:43:39 -0500

    done

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../Store/cart-context';
 
 const HeaderCartButton = (props) => {
+   const[btnHighlighted, setBtnHighlighted] = useState(false)
   const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
 
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+
+  const btnClasses =   `${classes.button}  ${btnHighlighted ? classes.bump :" "}`;
+
+  useEffect(() =>{
+    if(cartCtx.items.length === 0) {
+        return;
+    }
+  setBtnHighlighted(true);
+
+  const timer = setTimeout(() =>{
+    setBtnHighlighted(false);
+  },300);
+
+  return () => {
+    clearTimeout(timer)
+  }
+},[items])
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
